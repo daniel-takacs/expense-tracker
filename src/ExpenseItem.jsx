e4fd316f53a667225196747a45bee0fce2b5656c
daniel-takacs <takacsd89@gmail.com>
2021-03-11 18:59:47 +0200

    removeExpenses added

--- src/ExpenseItem.jsx
+++ src/ExpenseItem.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 
-function ExpenseItem({expenses, index}) {
+function ExpenseItem({expenses, index, removeExpenses}) {
+    
+ 
+    const clickHandle = (e) => {
+        e.preventDefault();
+        removeExpenses(index)
+    }
+  
     return (
         <div className="expense-item">
             <li>{expenses.textInput}</li>
             <li>{expenses.date}</li>
             <li>{expenses.amount}</li>
+            <button onClick={clickHandle}>x</button>
         </div>
     )
 }
--- src/ExpenseList.jsx
+++ src/ExpenseList.jsx
@@ -3,16 +3,25 @@ import ExpenseItem from './ExpenseItem'
 
 
 function ExpenseList({expenses, setExpenses}) {
-    
+    const removeExpenses = i => {
+        let newArr = expenses.filter((v, index) => index !== i)
+        setExpenses(newArr) 
+    }
     return (
         <div className="expense-list">
             <ul>
                 {expenses.map((value, index) => (
-                    <ExpenseItem key={index} expenses={value} index={index} />
+                    <ExpenseItem key={index} 
+                                 expenses={value} 
+                                 index={index} 
+                                 setExpenses={setExpenses}
+                                 removeExpenses={removeExpenses}/>
                 ))}
             </ul>
+            
         </div>
     )
+    
     }
 
 export default ExpenseList
